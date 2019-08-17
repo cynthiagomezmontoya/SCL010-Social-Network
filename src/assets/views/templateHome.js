ae360f3b7d4a59a9cb06817a51c2d11d15b13fd3
Adriana Sanchez <adrievelyn@gmail.com>
2019-08-16 20:33:56 -0400

    subiendo cambios de Alejandra

---
+++ src/assets/views/templateHome.js
@@ -0,0 +1,23 @@
+export const templateHome = () => {
+	const containerHome = document.createElement("div");
+	containerHome.className = "containerHome";
+	const contentHome = `
+   <section class="content">
+    <strong> <p>Tu Red Social, para compartir información, vivencias, Tips  y todo lo que quieras con mujeres de la Comunidad Tech.</p> </strong>      <img class="avatar" id="avatar" src="assets/img/usuario.png">
+   </section>
+
+    <div class="buttonPpal" id="containerLogin">
+      <a href="#/login"class= "buttons"> Iniciar Sesion</a>
+    </div>
+       <br>
+      <div class="buttonPpal" id="containerRegister">
+      <a href="#/register" class= "buttons"> Crear Cuenta </a>
+    </div>
+
+		`;
+	containerHome.innerHTML = contentHome;
+	return containerHome;
+}
+export const signOut = () => {
+	authSignOut();
+}
